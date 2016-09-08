739d344377f30bddfd8b4f2c64707d4821ccbd54
Bisheng Liu <b.liu@nki.nl>
2016-09-08 16:35:34 +0200

    working on adding features

--- seqViewer.js
+++ seqViewer.js
@@ -18,15 +18,17 @@ var features = [
 //name, start, end, color, clockwise, cut;
 
 //ntPerLine: the number of nt perline
-var ntPerLine = 180;
+var ntPerLine = 230;
 
 //generate complementary sequence
 var cSequence = genCSeq(sequence);
 
-
+//show complementary sequence
+var showComplementary = true;
+var symbol = ' ';
 //generate sequence array based on the ntPerLine
-var seqArray = formatSeq(sequence, ' ', ntPerLine); //array for forward sequence
-var cSeqArray = formatSeq(cSequence, ' ', ntPerLine);; //array for complementary sequence
+var seqArray = formatSeq(sequence, symbol, ntPerLine); //array for forward sequence
+var cSeqArray = formatSeq(cSequence, symbol, ntPerLine);; //array for complementary sequence
 
 
 //use d3 to display seq in text and as well as the features
@@ -34,12 +36,21 @@ var cSeqArray = formatSeq(cSequence, ' ', ntPerLine);; //array for complementary
 //draw canvas
 //seqWidth and featureWdith will be calculated later
 //define the seqWidth
-seqWidth = 50;
+var seqTop =5;
+var seqBottom = 5;
+
+var seqWidth = 20;
+//enzyme width
+var enzymeWidth = 10;
 //featureWdith 
-featureWdith = 50;
+var featureWdith = 20;
 var arrayLength = seqArray.length;
 //draw svg
-var svg = drawSVG("#displaySeq", arrayLength, seqWidth, featureWdith);
+var svg = drawSVG("#displaySeq", arrayLength, seqTop, enzymeWidth, seqWidth, featureWdith, seqBottom);
+
+//draw forward seq and complementary sequence
+var svgSeq= drawSeq(svg, seqArray, symbol, showComplementary, cSeqArray, seqTop, enzymeWidth, seqWidth, featureWdith, seqBottom, ntPerLine, features);
+
 
 //functions
 //render google color
@@ -82,10 +93,10 @@ function formatSeq(sequence, symbol, ntPerLine){
 }
 
 //draw empty using d3
-function drawSVG(id, arrayLength, seqWidth, featureWdith) { 
+function drawSVG(id, arrayLength, seqTop, enzymeWidth, seqWidth, featureWdith, seqBottom) { 
     var margin = {top: 20, right: 20, bottom: 20, left: 20};
     width = +$(id).width() - margin.left - margin.right;
-    height = arrayLength * (seqWidth + featureWdith) - margin.top - margin.bottom;
+    height = arrayLength * ( seqTop + enzymeWidth  + seqWidth + featureWdith + seqBottom) - margin.top - margin.bottom;
     //calculate the width
     var svg = d3.select(id)
                 .append("svg")
@@ -95,3 +106,198 @@ function drawSVG(id, arrayLength, seqWidth, featureWdith) {
                         .attr("transform", "translate(" + margin.left +"," + margin.top + ")");
     return svg;
  }
+
+
+
+ //draw sequence
+ function drawSeq(svg, seqArray, symbol, showComplementary, cSeqArray, seqTop, enzymeWidth, seqWidth, featureWdith, seqBottom, ntPerLine, features) { 
+     var yPos = 5;
+    var xShift = 25 // for adding count and vertical line
+    var count = 1;
+    //define 10nt width
+    var nt10 = 70.16;
+    var sp = 8.50;
+    svgSeq = svg.append("g").attr("id", "seqSVG");
+    for(i=0; i< seqArray.length; i++){
+        //forward sequence
+        var fcSeq = svgSeq.append("g");
+                    fcSeq.append("text")
+                        .attr("y", yPos)
+                        .attr("x", xShift)
+                        .style("text-anchor", "begin")
+                        .style("font-family", "monospace")
+                        .style("font-size", "13px")
+                        .style("fill", "#636363")
+                        .text(seqArray[i]);
+    //add nt count
+                    fcSeq.append("text")
+                        .attr("y", yPos)
+                        .attr("x", 0)
+                        .attr("class", "noEvent")
+                        .style("text-anchor", "middle")
+                        .style("font-family", "monospace")
+                        .style("font-size", "13px")
+                        .style("fill", "#636363")
+                        .text(count + "");
+    //show middle nt count
+    //first draw horizontal line
+    //remove the symbol in the seqArrayItem
+    var seqArrayItem = seqArray[i].split(symbol).join('');
+    if(seqArrayItem.length == ntPerLine){
+            for(c=0; c < Math.trunc(seqArrayItem.length / 10); c++){
+                var hline= fcSeq.append("g").append("line")
+                                .style("stroke", "#c7c7c7")
+                                .style("stroke-width", 0.5)
+                                .attr("x1", xShift + nt10/17 + c * (nt10 + sp))
+                                .attr("y1", yPos + 6) 
+                                .attr("x2", xShift + nt10 - nt10/20 + c * (nt10 + sp)) 
+                                .attr("y2", yPos + 6);
+            //draw small vertical lines
+            for(v=0; v < 10; v++){
+                var vline = fcSeq.append("g").append("line")
+                                .style("stroke-width", 0.5)
+                                .style("stroke", "#c7c7c7")
+                                .attr("x1", xShift + c * (nt10 + sp) + nt10/17 + v * nt10/10)
+                                .attr("y1", function () { return (v == 0 || v == 9 || v == 5) ? (yPos + 3) : (yPos + 4); }) 
+                                .attr("x2", xShift + c * (nt10 + sp)+ nt10/17 + v * nt10/10)
+                                .attr("y2", function () { return (v == 0 || v == 9 || v == 5) ? (yPos + 9) : (yPos + 8); });
+                        }
+            }
+    }
+    else{
+        for(c=0; c < Math.trunc(seqArrayItem.length / 10); c++){
+                var hline= fcSeq.append("g").append("line")
+                                .style("stroke", "#c7c7c7")
+                                .style("stroke-width", 0.5)
+                                .attr("x1", xShift + nt10/17 + c * (nt10 + sp))
+                                .attr("y1", yPos + 6) 
+                                .attr("x2", xShift + nt10 - nt10/20 + c * (nt10 + sp)) 
+                                .attr("y2", yPos + 6);
+                //draw small vertical lines
+                for(v=0; v < 10; v++){
+                    var vline = fcSeq.append("g").append("line")
+                                    .style("stroke-width", 0.5)
+                                    .style("stroke", "#c7c7c7")
+                                    .attr("x1", xShift + c * (nt10 + sp) + nt10/17 + v * nt10/10)
+                                    .attr("y1", function () { return (v == 0 || v == 9 || v == 5) ? (yPos + 3) : (yPos + 4); }) 
+                                    .attr("x2", xShift + c * (nt10 + sp)+ nt10/17 + v * nt10/10)
+                                    .attr("y2", function () { return (v == 0 || v == 9 || v == 5) ? (yPos + 9) : (yPos + 8); });
+                }
+            }
+        if(seqArrayItem.length % 10 !=0 ){
+            var ntLeft = seqArrayItem.length % 10;
+            var hline= fcSeq.append("g").append("line")
+                                .style("stroke", "#c7c7c7")
+                                .style("stroke-width", 0.5)
+                                .attr("x1", xShift + nt10/17 + Math.trunc(seqArrayItem.length / 10) * (nt10 + sp))
+                                .attr("y1", yPos + 6) 
+                                .attr("x2", xShift + nt10 * (ntLeft / 10 ) - nt10/20 + Math.trunc(seqArrayItem.length / 10)  * (nt10 + sp)) 
+                                .attr("y2", yPos + 6);
+                                //draw small vertical lines
+                for(v=0; v < ntLeft; v++){
+                    var vline = fcSeq.append("g").append("line")
+                                    .style("stroke-width", 0.5)
+                                    .style("stroke", "#c7c7c7")
+                                    .attr("x1", xShift + Math.trunc(seqArrayItem.length / 10) * (nt10 + sp) + nt10/17 + v * nt10/10)
+                                    .attr("y1", function () { return (v == 0 || v == 9 || v == 5) ? (yPos + 3) : (yPos + 4); }) 
+                                    .attr("x2", xShift + Math.trunc(seqArrayItem.length / 10) * (nt10 + sp)+ nt10/17 + v * nt10/10)
+                                    .attr("y2", function () { return (v == 0 || v == 9 || v == 5) ? (yPos + 9) : (yPos + 8); });
+                }
+        }
+    }
+    
+    //complementary sequence
+    if(showComplementary){
+                    fcSeq.append("text")
+                        .attr("class", "noEvent")
+                        .attr("y", yPos + seqWidth)
+                        .attr("x", xShift)
+                        .style("text-anchor", "begin")
+                        .style("font-family", "monospace")
+                        .style("font-size", "13px")
+                        .style("fill", "#c7c7c7")
+                        .text(cSeqArray[i]);
+    }                 
+    //cal the y pos based on features
+    //get current seq range
+    var seqStart = ntPerLine * i + 1;
+    var seqEnd = seqStart + ntPerLine;
+
+    //first get the features that cover the whole range, complete overlapping features
+    var coFeatures = $.grep(features, function (f, i) { 
+        return f.start <= seqStart && f.end >= seqEnd;
+     })
+     //draw coFeatures
+     var fWidth = 20;
+
+     //calculate the feature rect width
+     $('body').append('<div id="measure-text-width" class="nt-text-width">ATGCATGCGC</div>');
+     var rectWidth = $("#measure-text-width").text(seqArray[0]).width();
+     $("#measure-text-width").remove();
+
+     var coStep = 5; //vertical space between each feature
+
+     if(coFeatures.length >=1){
+     var feature = svgSeq.append("g");
+        for(f = 0; f < coFeatures.length; f++){
+            //feature rect
+            debugger;
+            var featureRect = feature.append("rect")
+                                .style("fill", coFeatures[f].color)
+                                .style("opacity", 0.8)
+                                .attr("x", xShift)
+                                .attr("y", yPos + seqWidth + coStep * (f+1))
+                                .attr("width", rectWidth )
+                                .attr("height", fWidth - 6);
+            //feature label background                 
+            var featureBg = feature.append("rect")
+                                .attr("class", "noEvent")
+                                .style("fill", "white")
+                                .style("opacity", 1)
+                                .attr("x", xShift + rectWidth / 2 - (coFeatures[f].name.split('').length + 10 ) * 7/2 )
+                                .attr("y", yPos + seqWidth + coStep * (f+1))
+                                .attr("width", (coFeatures[f].name.split('').length + 10  ) * 7 )
+                                .attr("height", fWidth - 6);
+            //feature label
+            var featureLabel = feature.append("text")
+                                .attr("class", "noEvent")
+                                .attr("y", yPos + seqWidth + coStep * (f+1) + fWidth / 2)
+                                .attr("x", xShift + rectWidth / 2)
+                                .style("text-anchor", "middle")
+                                .style("font-family", "monospace")
+                                .style("font-size", "10px")
+                                .style("fill", function () { return coFeatures[f].color; })
+                                .text(function(){return (coFeatures[f].clockwise===0? ">>>> " : "<<<< ") + coFeatures[f].name  + (coFeatures[f].clockwise===0? " >>>>" : " <<<<"); });
+        }
+     }
+     
+     var fNewWdith =  fWidth * coFeatures.length;
+
+    //  //get all the features located in the range or partially overlapped
+    //  var poFeatures = $.grep(features, function (f, i) { 
+    //     return ((f.start > seqStart && f.end < seqEnd) ||(f.start < seqStart && f.end < seqEnd) || (f.start > seqStart && f.end > seqEnd));
+    //  })
+    //  if(poFeatures.length >0){
+    //      //draw the features based on the start, need to deal with the overlapping features
+    //  }
+
+     featureHeight = fNewWdith === 0 ? featureWdith : fNewWdith;
+    //cal the y pos based on enzymes
+
+    yPos = yPos + seqTop + enzymeWidth  + seqWidth + featureHeight + seqBottom;
+    //cal the count 
+    count = count + ntPerLine;
+    }
+
+    //draw vertical line after ntcount
+    svgSeq.append("line")
+            .style("stroke", "#c7c7c7")
+            .attr("x1", xShift -5)     // x position of the first end of the line
+            .attr("y1", 5 - enzymeWidth- seqTop)             // y position of the first end of the line
+            .attr("x2", xShift -5)     // x position of the second end of the line
+            .attr("y2", yPos);    // y position of the second end of the line
+
+    return svgSeq;
+  }
+
+
